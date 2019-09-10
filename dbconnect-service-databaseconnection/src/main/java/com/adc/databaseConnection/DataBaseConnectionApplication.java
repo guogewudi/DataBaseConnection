fab088f9d4c826e51cd538397928cf5679dcfce4
guoge <1240780374@qq.com>
2019-09-10 14:57:21 +0800

    success

--- dbconnect-service-databaseconnection/src/main/java/com/adc/databaseConnection/DataBaseConnectionApplication.java
+++ dbconnect-service-databaseconnection/src/main/java/com/adc/databaseConnection/DataBaseConnectionApplication.java
@@ -23,5 +23,23 @@ public class DataBaseConnectionApplication {
 
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("PUT", "DELETE","GET","POST")
+                        .allowedHeaders("*")
+                        .exposedHeaders("access-control-allow-headers",
+                                "access-control-allow-methods",
+                                "access-control-allow-origin",
+                                "access-control-max-age",
+                                "X-Frame-Options")
+                        .allowCredentials(true).maxAge(3600);
+            }
+        };
+    }
 
 }
